66c3b55fdfb4cf564d07a44de57f1720e5dfc1da
VuPlayBoizz <nguyenbavu1902@gmail.com>
2024-06-09 15:50:24 +0700

    Trigonometric

---
+++ Week 1/Trigonometric.py
@@ -0,0 +1,58 @@
+import math
+
+def factorial(n):
+    total = 1
+    while n != 0:
+        total = total * n
+        n = n - 1
+    return total
+
+def approx_sin(x, n):
+    if n <= 0:
+        print("n must be greater than 0")
+        return
+    else:
+        ans = 0
+        for i in range(0, n):
+            cal = ((-1) ** i) * ((x ** (2 * i + 1)) / factorial(2 * i + 1))
+            ans += cal
+        print(ans)
+
+def approx_cos(x, n):
+    if n <= 0:
+        print("n must be greater than 0")
+        return
+    else:
+        ans = 0
+        for i in range(0, n):
+            cal = ((-1) ** i) * ((x ** (2 * i)) / factorial(2 * i))
+            ans += cal
+        print(ans)
+
+def approx_sinh(x, n):
+    if n <= 0:
+        print("n must be greater than 0")
+        return
+    else:
+        ans = 0
+        for i in range(0, n):
+            cal = (x ** (2 * i + 1)) / factorial(2 * i + 1)
+            ans += cal
+        print(ans)
+
+def approx_cosh(x, n):
+    if n <= 0:
+        print("n must be greater than 0")
+        return
+    else:
+        ans = 0
+        for i in range(0, n):
+            cal = (x ** (2 * i)) / factorial(2 * i)
+            ans += cal
+        print(ans)
+
+if __name__ == '__main__':
+    approx_sin(x = 3.14, n = 10)
+    approx_cos(x = 3.14, n = 10)
+    approx_sinh(x = 3.14, n = 10)
+    approx_cosh(x = 3.14, n = 10)
