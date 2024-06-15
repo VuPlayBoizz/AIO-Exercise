eb0f3f8b21acb374e22ea2ce7395919659949761
VuPlayBoizz <nguyenbavu1902@gmail.com>
2024-06-15 22:22:38 +0700

    Cập nhật Exercice_5-Mean_difference.py

--- Week 1/Exercice_5-Mean_difference.py
+++ Week 1/Exercice_5-Mean_difference.py
@@ -1,12 +1,13 @@
-def md_nre_single_sample (y, y_hat, n, p):
+def md_nre_single_sample(y, y_hat, n, p):
     y_root = y ** (1/n)
     y_hat_root = y_hat ** (1/n)
     loss = (y_root - y_hat_root) ** p
-    
+
     return print(loss)
 
+
 if __name__ == '__main__':
-    md_nre_single_sample(y =100 , y_hat =99.5 , n =2 , p =1)
-    md_nre_single_sample ( y =50 , y_hat =49.5 , n =2 , p =1)
-    md_nre_single_sample ( y =20 , y_hat =19.5 , n =2 , p =1)
-    md_nre_single_sample ( y =0.6 , y_hat =0.1 , n =2 , p =1)
+    md_nre_single_sample(y=100, y_hat=99.5, n=2, p=1)
+    md_nre_single_sample(y=50, y_hat=49.5, n=2, p=1)
+    md_nre_single_sample(y=20, y_hat=19.5, n=2, p=1)
+    md_nre_single_sample(y=0.6, y_hat=0.1, n=2, p=1)
