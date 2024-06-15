220448ec724a3915731f845512596f428c3856f4
VuPlayBoizz <nguyenbavu1902@gmail.com>
2024-06-15 22:15:23 +0700

    Lưu thay đổi đối với Exercice_5-Mean_difference.py

--- Week 1/Exercice_5-Mean_difference.py
+++ Week 1/Exercice_5-Mean_difference.py
@@ -1,9 +1,9 @@
 def md_nre_single_sample(y, y_hat, n, p):
     y_root = y ** (1/n)
     y_hat_root = y_hat ** (1/n)
-    Loss = (y_root - y_hat_root) ** p
+    loss = (y_root - y_hat_root) ** p
 
-    return print(Loss)
+    return print(loss)
 
 
 if __name__ == '__main__':
